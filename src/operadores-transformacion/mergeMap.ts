2acabfcdc227d5d75cfea6d49b95f7ee43ec2ea0
Deymer Hernández <deymerh@hotmail.com>
2023-01-04 16:34:33 -0500

    mergeMap

---
+++ src/operadores-transformacion/mergeMap.ts
@@ -0,0 +1,24 @@
+import { of, mergeMap, interval, map, Observer, take, fromEvent, takeUntil } from 'rxjs';
+ 
+const observer: Observer<any> = {
+  next: (valor) => console.log('Next ', valor),
+  error: (error) => console.warn('Error: ', error),
+  complete: () => console.info('Completado')
+}
+
+const letters = of('a', 'b', 'c');
+const result = letters.pipe(
+  mergeMap((letter) => interval(1000).pipe(
+    map(interval => letter + interval),
+    take(3)
+  ))
+);
+// result.subscribe(observer);
+
+const mouseDown$ = fromEvent(document, 'mousedown'); 
+const mouseUp$ = fromEvent(document, 'mouseup'); 
+const interval$ = interval(); 
+
+mouseDown$.pipe(
+  mergeMap(()=>interval$.pipe(takeUntil(mouseUp$)))
+).subscribe(console.log);
